872fa53bd7c0dcb7e8b500930937d83f2bdbb3a9
yuuki1210 <aile175@outlook.jp>
2021-09-23 10:51:36 +0900

    全然、シュミレーターの画面が変わらないバグがあるのでは？ 分からん。

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -16,16 +16,22 @@ class _HomepageState extends State<Homepage> {
           padding:EdgeInsets.symmetric(
             horizontal: 24.0,
             vertical: 32.0,
-          )
+          ),
+         color: Color(0xFFF6F6F6),
          child:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image(
-              image:AssetImage(
-                'assets/images/logo.png'
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 32.0
               ),
-            )
-            TaskCardWidget(),
+              child: Image(
+                image:AssetImage(
+                  'assets/images/logo.png'
+                ),
+              ),
+            ),
+            TaskCardwidget(),
           ],
          ),
         ),
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class  TaskCardWidget extends StatefulWidget {
-  @overrride
-  Widget build(BuildContext context)
+class TaskCardwidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child:Text("Hellow world!"),
-      );
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        vertical: 32.0,
+        horizontal: 24.0,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Get Started!",
+            style: TextStyle(
+              color: Color(0xFF211551),
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+          Text(
+            "Hello user! welcome to what_TODO app, this is a default task that you can edit or delete to start using the app.",
+            style: TextStyle(
+              fontSize: 16.0
+            ),
+          )
+      ],
+      ),
+    );
   }
 }
